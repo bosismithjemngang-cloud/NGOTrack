8324b502cca8117d9d13892d60b997755016b092
BOSI SMITH JEMNGANG <bosismithjemngang@gmail.com>
2026-04-09 13:00:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8057118093-2664f",
+  "appId": "1:51032163844:web:777ec0e29279d50fdb9751",
+  "apiKey": "AIzaSyAA9KyqaXNQsNrgRx9cBwBBjKNUgge6SwM",
+  "authDomain": "studio-8057118093-2664f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "51032163844"
+};
